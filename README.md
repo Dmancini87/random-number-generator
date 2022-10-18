1b0af3d81d9faefc8e2b0036543f3c115eb19a40
david <dmancini87@gmail.com>
2022-10-17 20:58:31 -0400

    final code

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Password Generator Starter Code
+password gerrator
+Week three Project
+by David Mancini 
+The third weekly project for the UNH coding bootcamp. This website will create a random password, accourding to options you choose
+Install
+Open the website in a browser, imput the number of chracaters, and select if you want symbols, numbers, uppercase, and or lowercase. then gererate the password
+view the code and css style page 
+
+Usage
+THIS PROJECT ISNT COMPLETE YET, I WILL FINISH IT
+Licese
+Unlicensed
--- script.js
+++ script.js
@@ -10,33 +10,65 @@ const lower = "abcdefghijklmnopqrstuzwxyz"
 const number = "0123456789"
 const symbol = "!@#$%^&*_+-="
 
-function generatePassword(){
-return Math.floor(Math.random() * charaNum);
-if(symbChoice === true){
-}
+function generatePassword() {
+   var password = "";
+for (var i = 0; i <= charaNum; i++) {
+  if(lowerChoice === true){
+ var randomLower = Math.floor(Math.random() * lower.length);
+ password += lower.substring(randomLower, randomLower +1);
+  }
+  if(upperChoice === true){
+ var randomUpper = Math.floor(Math.random() * upper.length);
+ password += upper.substring(randomUpper, randomUpper +1);
+  }
+  if(symbChoice === true){
+    var randomSymb = Math.floor(Math.random() * symbol.length);
+    password += symbol.substring(randomSymb, randomSymb +1);
+     }
 if(numChoice === true){
-
+    var randomNum = Math.floor(Math.random() * number.length);
+    password += number.substring(randomNum, randomNum +1);
+     }
 }
-if(upperChoice === true){
-
-}
-if(LowerChoice === true){
-
+      document.getElementById("password").value = password;
+      return password;
 }
 
-}
+// function generatePassword(){
+// return Math.floor(Math.random() * );
+// if(symbChoice === true){
+// password.concat(symbol, number, upper, lower)
+  // }
+// if(numChoice === true){
+
+// }
+// if(upperChoice === true){
+
+// }
+// if(lowerChoice === true){
+
+// }
+
+// }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var randomPassword = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = randomPassword;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// let password = ""
+// const upper = "ABC"
+// const lower = "abc"
+
+// console.log (password.concat(upper, lower))
+
